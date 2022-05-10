522d56ada6f62dc8f81266d99c47bdb297efd286
Erik Siefken <git@posporus.de>
2022-05-10 10:29:56 +0200

    added oak logging and error handling

--- examples/food-diary/app.ts
+++ examples/food-diary/app.ts
@@ -1,13 +1,72 @@
 import {
-    Application
+    Application,
+    HttpError,
+    Status
 } from './deps.ts'
 
-import {apiRouter} from './routes/api.router.ts'
+import {
+    bold,
+    cyan,
+    green,
+    red,
+    yellow,
+} from 'https://deno.land/std@0.131.0/fmt/colors.ts'
+
+import { apiRouter } from './routes/api.router.ts'
 
 const app = new Application();
 
 
 app.use(apiRouter.routes())
+
+// Error handler middleware
+app.use(async (context, next) => {
+    try {
+        await next();
+    } catch (e) {
+        if (e instanceof HttpError) {
+            // deno-lint-ignore no-explicit-any
+            context.response.status = e.status as any;
+            if (e.expose) {
+                context.response.body = `<!DOCTYPE html>
+              <html>
+                <body>
+                  <h1>${e.status} - ${e.message}</h1>
+                </body>
+              </html>`;
+            } else {
+                context.response.body = `<!DOCTYPE html>
+              <html>
+                <body>
+                  <h1>${e.status} - ${Status[e.status]}</h1>
+                </body>
+              </html>`;
+            }
+        } else if (e instanceof Error) {
+            context.response.status = 500;
+            context.response.body = `<!DOCTYPE html>
+              <html>
+                <body>
+                  <h1>500 - Internal Server Error</h1>
+                </body>
+              </html>`;
+            console.log("Unhandled Error:", red(bold(e.message)));
+            console.log(e.stack);
+        }
+    }
+});
+
+// Logger
+app.use(async (context, next) => {
+    await next();
+    const rt = context.response.headers.get("X-Response-Time");
+    console.log(
+        `${green(context.request.method)} ${cyan(context.request.url.pathname)} - ${bold(
+            String(rt),
+        )
+        }`,
+    );
+});
 
 app.use(async (context, next) => {
     try {
